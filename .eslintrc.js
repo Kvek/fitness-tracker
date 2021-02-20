d92956a4d724c3f608ac5d55af178ce4195ac23c
Vivek <vkavek@gmail.com>
2021-02-20 23:06:00 +0000

    Added styled-components and import sort packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
     "plugin:react/recommended",
     "airbnb",
     "plugin:prettier/recommended",
+    "prettier/react",
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
@@ -16,16 +17,32 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: "module",
   },
-  plugins: ["react", "@typescript-eslint"],
+  plugins: ["react", "@typescript-eslint", "simple-import-sort", "prettier"],
   rules: {
     "react/jsx-filename-extension": [
       1,
-      { extensions: [".js", ".jsx", ".tsx"] },
+      { extensions: [".js", ".jsx", ".ts", ".tsx"] },
     ],
     "no-use-before-define": "off",
     "@typescript-eslint/no-use-before-define": ["error"],
     "import/prefer-default-export": 0,
     "import/no-unresolved": 0,
     "import/extensions": 0,
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
+    "sort-imports": "off",
+    "import/order": "off",
+    "react/jsx-uses-react": "off",
+    "react/react-in-jsx-scope": "off",
+    "prettier/prettier": "error",
   },
+  overrides: [
+    {
+      files: "./src/**/*.{js,jsx,ts,tsx}",
+      rules: {
+        "simple-import-sort/imports": "off",
+        "import/order": ["error", { "newlines-between": "always" }],
+      },
+    },
+  ],
 };
